b80a239bc7064441c57af27488575072647697ee
akhxlrana <akhilrana369@gmail.com>
2024-09-18 20:35:07 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+
+function Tours({tours,removeTour}){
+    return(
+        <div className="container">
+            <div className="cards">
+                {
+                  tours.map((tour)=> {
+                    return <Card{...tour} removeTour={removeTour}></Card>
+                  
+                    } )
+                }
+            </div>
+        </div>
+    )
+}
+export default Tours;
